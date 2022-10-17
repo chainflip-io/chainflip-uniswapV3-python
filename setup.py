e60deb10d1cf7ae86427dee93af641b532629566
albert <allimos3@gmail.com>
2022-10-17 13:59:20 +0200

    chore: update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pythonUniswapV3",
-    version="0.0.4",
+    version="0.0.1",
     author="Chainflip Labs",
     author_email="albert@chainflip.io",
     description="Pythonized Uniswap V3",
